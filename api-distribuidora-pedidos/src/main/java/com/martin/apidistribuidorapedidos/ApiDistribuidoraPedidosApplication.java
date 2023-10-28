6776b4574c8158fbdb577ff8c1ea53929cefa1e4
marcos <martinmarcospablo@gmail.com>
2023-10-28 18:18:13 -0300

    Día uno

---
+++ api-distribuidora-pedidos/src/main/java/com/martin/apidistribuidorapedidos/ApiDistribuidoraPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.martin.apidistribuidorapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDistribuidoraPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDistribuidoraPedidosApplication.class, args);
+	}
+
+}
